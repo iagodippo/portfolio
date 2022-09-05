17d864b31ce37716c785641985564610d510265d
Iago D'Ippolito <iagodippolito@hotmail.com>
2022-09-05 18:40:04 -0300

    terceiro

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 window.onscroll = function() {scrollFunction()};
 
+// função para o scroll
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
@@ -9,7 +10,7 @@ function scrollFunction() {
     document.getElementById("myBtn").style.display = "none";
   }
 }
-
+// função para voltar ao topo
 function topFunction() {
     document.documentElement.scrollTop = 0;
 }
